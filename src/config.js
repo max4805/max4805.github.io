d2e6ec81d3d1b1a0442276e0445a1877c503e589
max4805 <max480@free.fr>
2022-08-27 20:41:10 +0200

    Push a clone of the website

---
+++ src/config.js
@@ -0,0 +1,4 @@
+export default {
+  // backendUrl: "http://localhost:8080",
+  backendUrl: "https://max480-random-stuff.appspot.com",
+};
---
+++ src/views/RouteNotFound.vue
@@ -0,0 +1,14 @@
+<script>
+import router from "../router";
+
+const vue = {
+  name: "route-not-found",
+
+  // redirect to Banana Mirror Browser, since this is the only page on that website
+  mounted: function () {
+    router.push('/celeste/banana-mirror-browser')
+  },
+};
+
+export default vue;
+</script>
